48b88537077363767a555ac220a0a46bf9a20919
Wimpy Katana <wim.katana@gmail.com>
2025-01-21 10:07:16 +0700

    update

--- section 04/01_list.py
+++ section 04/01_list.py
@@ -4,17 +4,17 @@ mixed = [1, 'apple', True, 3.14]
 
 print(numbers[2])  # 3
 print(fruits[1])  # banana
+print(mixed[3])  # 3.14
+print(fruits[-1]) # elderberry
 
 slice_fruits = fruits[1:4]
 print(slice_fruits)  # ['banana', 'cherry', 'date']
 
-print(mixed[3])  # 3.14
-
 print(fruits[-1])  # elderberry
 
 fruits.append('grape')
+print(fruits) # ['apple', 'banana', 'cherry', 'date', 'elderberry', 'grape']
 fruits.insert(2, 'manggo')
-
 print(fruits)  # ['apple', 'banana', 'manggo', 'cherry', 'date', 'elderberry', 'grape']
 
 fruits.remove('banana')
